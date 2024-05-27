700222cba81a1e3c0cd19d443c5c15c342962d8a
Kurch00 <michaelkurchevski@gmail.com>
2024-05-27 12:19:01 -0300

    Alterei o main

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,58 @@
+import { faStar, faTrashCan } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import DarthVader from './img/darthvader.jpg'
 
 function Main(){
     return(
         <div class="main">
             <h1>Personagens</h1>
+            <div class="cards-main" id='cards-main'>
+                
+                <div class="card">
+                    <div class="cima-card">
+                        <button><FontAwesomeIcon icon={faStar}/></button>
+                        <button><FontAwesomeIcon icon={faTrashCan}/></button>
+                    </div>
+                    <div class="meio-card">
+                        <img src={DarthVader} />
+                    </div>
+                    <div class="baixo-card">
+                        <h2>Darth Vader</h2>
+                        <span>Vivo - Humano</span>
+                        <button>Ver detalhes</button>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="cima-card">
+                        <button><FontAwesomeIcon icon={faStar}/></button>
+                        <button><FontAwesomeIcon icon={faTrashCan}/></button>
+                    </div>
+                    <div class="meio-card">
+                        <img src={DarthVader} />
+                    </div>
+                    <div class="baixo-card">
+                        <h2>Darth Vader</h2>
+                        <span>Vivo - Humano</span>
+                        <button>Ver detalhes</button>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="cima-card">
+                        <button><FontAwesomeIcon icon={faStar}/></button>
+                        <button><FontAwesomeIcon icon={faTrashCan}/></button>
+                    </div>
+                    <div class="meio-card">
+                        <img src={DarthVader} />
+                    </div>
+                    <div class="baixo-card">
+                        <h2>Darth Vader</h2>
+                        <span>Vivo - Humano</span>
+                        <button>Ver detalhes</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
